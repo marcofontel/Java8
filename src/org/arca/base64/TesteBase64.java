5423df121d0b24ed1a0a2a8a4a57a4b320694bfc
server <wll.rgr@gmail.com>
2022-12-01 18:49:17 -0300

    Ajustes finais

--- src/org/arca/base64/TesteBase64.java
+++ src/org/arca/base64/TesteBase64.java
@@ -1,5 +1,26 @@
 package org.arca.base64;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
 public class TesteBase64 {
 
+	static String nome = "Marcos é um cara qualquer!";
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Texto Original: "+nome);
+		try {
+			String codificado = Base64.getEncoder().encodeToString(nome.getBytes("utf-8"));
+			 // Codificar URL
+			Base64.getUrlEncoder();
+			
+			System.out.println("Codificado: "+codificado);
+			String decodificad = new String(Base64.getDecoder().decode(codificado), "utf-8");
+			System.out.println("Decodificado: "+decodificad);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
