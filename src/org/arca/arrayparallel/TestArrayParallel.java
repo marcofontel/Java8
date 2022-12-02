6dc7c8fd4eed284ac4bd4f543b31e09ca8b236d5
server <wll.rgr@gmail.com>
2022-12-02 06:00:11 -0300

    Paramentro de ordenação

--- src/org/arca/arrayparallel/TestArrayParallel.java
+++ src/org/arca/arrayparallel/TestArrayParallel.java
@@ -6,7 +6,7 @@ public class TestArrayParallel {
 
 	public static void main(String[] args) {
 		int[] numeros = {10, 34, 1, 35, 3, 2, 11, -2};
-		Arrays.parallelSort(numeros);
+		Arrays.parallelSort(numeros, 0,8);
 		for(int numero: numeros) {
 			System.out.println(numero + " ");
 		}
