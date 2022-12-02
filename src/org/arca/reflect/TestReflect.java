909b59f906b40de1fd2e2335261097572d81b60d
server <wll.rgr@gmail.com>
2022-12-02 07:47:52 -0300

    Reflexão codigo inspecionando codigo

--- src/org/arca/reflect/TestReflect.java
+++ src/org/arca/reflect/TestReflect.java
@@ -1,5 +1,24 @@
 package org.arca.reflect;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+import org.arca.lambda.Produto;
+
 public class TestReflect {
 
+	public static void main(String[] args) {
+		Produto p = new Produto("Geladeira", "3000");
+		Class cl = p.getClass();
+		
+		Method[] metodos = cl.getDeclaredMethods();
+		for(Method m : metodos) {
+			System.out.println(m.getName());
+			Parameter[] pr = m.getParameters();
+			for(Parameter pp: pr) {
+				System.out.println(pp);
+			}
+		}
+		
+	}
 }
