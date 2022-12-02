1c20bbad86031a73f00adf8cba609473ab822b4a
server <wll.rgr@gmail.com>
2022-12-02 05:55:56 -0300

    Ordenando com Parallel Sort

--- src/org/arca/arrayparallel/TestArrayParallel.java
+++ src/org/arca/arrayparallel/TestArrayParallel.java
@@ -1,12 +1,14 @@
 package org.arca.arrayparallel;
 
+import java.util.Arrays;
+
 public class TestArrayParallel {
 
 	public static void main(String[] args) {
-		int[] numeros = {10, 34, 1, 35, 3, 2, 11};
-
-		for(int numero : numeros) {
-			System.out.println(numero);
+		int[] numeros = {10, 34, 1, 35, 3, 2, 11, -2};
+		Arrays.parallelSort(numeros);
+		for(int numero: numeros) {
+			System.out.println(numero + " ");
 		}
 	}
 }
