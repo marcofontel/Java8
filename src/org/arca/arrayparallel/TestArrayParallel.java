2a443f03e975885a8a225099e991cce65100b71f
server <wll.rgr@gmail.com>
2022-12-02 05:52:40 -0300

    Inicial Array Parallel Sort

---
+++ src/org/arca/arrayparallel/TestArrayParallel.java
@@ -0,0 +1,12 @@
+package org.arca.arrayparallel;
+
+public class TestArrayParallel {
+
+	public static void main(String[] args) {
+		int[] numeros = {10, 34, 1, 35, 3, 2, 11};
+
+		for(int numero : numeros) {
+			System.out.println(numero);
+		}
+	}
+}
